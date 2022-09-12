7d50cb98ae32c3f56371cfd36e9ec3013de05e0d
Connor Watts <67272820+ConnorWatts@users.noreply.github.com>
2022-09-12 01:24:42 +0100

    Create generator.py

---
+++ model/generator.py
@@ -0,0 +1,274 @@
+### models.generator.py
+
+
+
+### GEBM Generators
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+import math
+import numpy as np
+
+from torchvision.transforms import Compose, Resize, ToTensor
+from einops import rearrange, reduce, repeat
+from einops.layers.torch import Rearrange, Reduce
+from torchsummary import summary
+
+
+class GeneratorTTS(nn.Module):
+    def __init__(self, seq_len=64, patch_size=15, channels=4, num_classes=9, latent_dim=100, embed_dim=10, depth=3,
+                 num_heads=5, forward_drop_rate=0.5, attn_drop_rate=0.5):
+        super(GeneratorTTS, self).__init__()
+        self.channels = channels
+        self.latent_dim = latent_dim
+        self.seq_len = seq_len
+        self.embed_dim = embed_dim
+        self.patch_size = patch_size
+        self.depth = depth
+        self.attn_drop_rate = attn_drop_rate
+        self.forward_drop_rate = forward_drop_rate
+        
+        self.l1 = nn.Linear(self.latent_dim, self.seq_len * self.embed_dim)
+        self.pos_embed = nn.Parameter(torch.zeros(1, self.seq_len, self.embed_dim))
+        self.blocks = Gen_TransformerEncoder(
+                         depth=self.depth,
+                         emb_size = self.embed_dim,
+                         drop_p = self.attn_drop_rate,
+                         forward_drop_p=self.forward_drop_rate
+                        )
+
+        self.deconv = nn.Sequential(
+            nn.Conv2d(self.embed_dim, self.channels, 1, 1, 0)
+        )
+
+    def forward(self, z):
+        x = self.l1(z).view(-1, self.seq_len, self.embed_dim)
+        x = x + self.pos_embed
+        H, W = 1, self.seq_len
+        x = self.blocks(x)
+        x = x.reshape(x.shape[0], 1, x.shape[1], x.shape[2])
+        output = self.deconv(x.permute(0, 3, 1, 2))
+        output = output.view(-1, self.channels, H, W)
+        return output
+    
+    
+class Gen_TransformerEncoderBlock(nn.Sequential):
+    def __init__(self,
+                 emb_size,
+                 num_heads=5,
+                 drop_p=0.5,
+                 forward_expansion=4,
+                 forward_drop_p=0.5):
+        super().__init__(
+            ResidualAdd(nn.Sequential(
+                nn.LayerNorm(emb_size),
+                MultiHeadAttention(emb_size, num_heads, drop_p),
+                nn.Dropout(drop_p)
+            )),
+            ResidualAdd(nn.Sequential(
+                nn.LayerNorm(emb_size),
+                FeedForwardBlock(
+                    emb_size, expansion=forward_expansion, drop_p=forward_drop_p),
+                nn.Dropout(drop_p)
+            )
+            ))
+
+        
+class Gen_TransformerEncoder(nn.Sequential):
+    def __init__(self, depth=8, **kwargs):
+        super().__init__(*[Gen_TransformerEncoderBlock(**kwargs) for _ in range(depth)])       
+        
+        
+class MultiHeadAttention(nn.Module):
+    def __init__(self, emb_size, num_heads, dropout):
+        super().__init__()
+        self.emb_size = emb_size
+        self.num_heads = num_heads
+        self.keys = nn.Linear(emb_size, emb_size)
+        self.queries = nn.Linear(emb_size, emb_size)
+        self.values = nn.Linear(emb_size, emb_size)
+        self.att_drop = nn.Dropout(dropout)
+        self.projection = nn.Linear(emb_size, emb_size)
+
+    def forward(self, x: Tensor, mask: Tensor = None) -> Tensor:
+        queries = rearrange(self.queries(x), "b n (h d) -> b h n d", h=self.num_heads)
+        keys = rearrange(self.keys(x), "b n (h d) -> b h n d", h=self.num_heads)
+        values = rearrange(self.values(x), "b n (h d) -> b h n d", h=self.num_heads)
+        energy = torch.einsum('bhqd, bhkd -> bhqk', queries, keys)  # batch, num_heads, query_len, key_len
+        if mask is not None:
+            fill_value = torch.finfo(torch.float32).min
+            energy.mask_fill(~mask, fill_value)
+
+        scaling = self.emb_size ** (1 / 2)
+        att = F.softmax(energy / scaling, dim=-1)
+        att = self.att_drop(att)
+        out = torch.einsum('bhal, bhlv -> bhav ', att, values)
+        out = rearrange(out, "b h n d -> b n (h d)")
+        out = self.projection(out)
+        return out
+
+    
+class ResidualAdd(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        res = x
+        x = self.fn(x, **kwargs)
+        x += res
+        return x
+    
+    
+class FeedForwardBlock(nn.Sequential):
+    def __init__(self, emb_size, expansion, drop_p):
+        super().__init__(
+            nn.Linear(emb_size, expansion * emb_size),
+            nn.GELU(),
+            nn.Dropout(drop_p),
+            nn.Linear(expansion * emb_size, emb_size),
+        )
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class GeneratorCRNN(nn.Module):
+    ''' C-RNN-GAN generator
+    '''
+    def __init__(self, num_feats, hidden_units=256, drop_prob=0.6, use_cuda=False):
+        super(GeneratorCRNN, self).__init__()
+
+        # params
+        self.hidden_dim = hidden_units
+        self.use_cuda = use_cuda
+        self.num_feats = num_feats
+
+        self.fc_layer1 = nn.Linear(in_features=(num_feats*2), out_features=hidden_units)
+        self.lstm_cell1 = nn.LSTMCell(input_size=hidden_units, hidden_size=hidden_units)
+        self.dropout = nn.Dropout(p=drop_prob)
+        self.lstm_cell2 = nn.LSTMCell(input_size=hidden_units, hidden_size=hidden_units)
+        self.fc_layer2 = nn.Linear(in_features=hidden_units, out_features=num_feats)
+
+    def forward(self, z, states):
+        ''' Forward prop
+        '''
+        if self.use_cuda:
+            z = z.cuda()
+        # z: (batch_size, seq_len, num_feats)
+        # z here is the uniformly random vector
+        batch_size, seq_len, num_feats = z.shape
+
+        # split to seq_len * (batch_size * num_feats)
+        z = torch.split(z, 1, dim=1)
+        z = [z_step.squeeze(dim=1) for z_step in z]
+
+        # create dummy-previous-output for first timestep
+        prev_gen = torch.empty([batch_size, num_feats]).uniform_()
+        if self.use_cuda:
+            prev_gen = prev_gen.cuda()
+
+        # manually process each timestep
+        state1, state2 = states # (h1, c1), (h2, c2)
+        gen_feats = []
+        for z_step in z:
+            # concatenate current input features and previous timestep output features
+            concat_in = torch.cat((z_step, prev_gen), dim=-1)
+            out = F.relu(self.fc_layer1(concat_in))
+            h1, c1 = self.lstm_cell1(out, state1)
+            h1 = self.dropout(h1) # feature dropout only (no recurrent dropout)
+            h2, c2 = self.lstm_cell2(h1, state2)
+            prev_gen = self.fc_layer2(h2)
+            # prev_gen = F.relu(self.fc_layer2(h2)) #DEBUG
+            gen_feats.append(prev_gen)
+
+            state1 = (h1, c1)
+            state2 = (h2, c2)
+
+        # seq_len * (batch_size * num_feats) -> (batch_size * seq_len * num_feats)
+        gen_feats = torch.stack(gen_feats, dim=1)
+
+        states = (state1, state2)
+        return gen_feats, states
+
+    def init_hidden(self, batch_size):
+        ''' Initialize hidden state '''
+        # create NEW tensor with SAME TYPE as weight
+        weight = next(self.parameters()).data
+
+        if (self.use_cuda):
+            hidden = ((weight.new(batch_size, self.hidden_dim).zero_().cuda(),
+                       weight.new(batch_size, self.hidden_dim).zero_().cuda()),
+                      (weight.new(batch_size, self.hidden_dim).zero_().cuda(),
+                       weight.new(batch_size, self.hidden_dim).zero_().cuda()))
+        else:
+            hidden = ((weight.new(batch_size, self.hidden_dim).zero_(),
+                       weight.new(batch_size, self.hidden_dim).zero_()),
+                      (weight.new(batch_size, self.hidden_dim).zero_(),
+                       weight.new(batch_size, self.hidden_dim).zero_()))
+
+        return hidden
+
+
+class DiscriminatorCRNN(nn.Module):
+    ''' C-RNN-GAN discrminator
+    '''
+    def __init__(self, num_feats, hidden_units=256, drop_prob=0.6, use_cuda=False):
+
+        super(DiscriminatorCRNN, self).__init__()
+
+        # params
+        self.hidden_dim = hidden_units
+        self.num_layers = 2
+        self.use_cuda = use_cuda
+
+        self.dropout = nn.Dropout(p=drop_prob)
+        self.lstm = nn.LSTM(input_size=num_feats, hidden_size=hidden_units,
+                            num_layers=self.num_layers, batch_first=True, dropout=drop_prob,
+                            bidirectional=True)
+        self.fc_layer = nn.Linear(in_features=(2*hidden_units), out_features=1)
+
+    def forward(self, note_seq, state):
+        ''' Forward prop
+        '''
+        if self.use_cuda:
+            note_seq = note_seq.cuda()
+
+        # note_seq: (batch_size, seq_len, num_feats)
+        drop_in = self.dropout(note_seq) # input with dropout
+        # (batch_size, seq_len, num_directions*hidden_size)
+        lstm_out, state = self.lstm(drop_in, state)
+        # (batch_size, seq_len, 1)
+        out = self.fc_layer(lstm_out)
+        out = torch.sigmoid(out)
+
+        num_dims = len(out.shape)
+        reduction_dims = tuple(range(1, num_dims))
+        # (batch_size)
+        out = torch.mean(out, dim=reduction_dims)
+
+        return out, lstm_out, state
+
+    def init_hidden(self, batch_size):
+        ''' Initialize hidden state '''
+        # create NEW tensor with SAME TYPE as weight
+        weight = next(self.parameters()).data
+
+        layer_mult = 2 # for being bidirectional
+        
+        if self.use_cuda:
+            hidden = (weight.new(self.num_layers * layer_mult, batch_size,
+                                 self.hidden_dim).zero_().cuda(),
+                      weight.new(self.num_layers * layer_mult, batch_size,
+                                 self.hidden_dim).zero_().cuda())
+        else:
+            hidden = (weight.new(self.num_layers * layer_mult, batch_size,
+                                 self.hidden_dim).zero_(),
+                      weight.new(self.num_layers * layer_mult, batch_size,
+                                 self.hidden_dim).zero_())
+        
+        return hidden
+
